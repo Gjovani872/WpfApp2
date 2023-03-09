ca7704b216514afe07935a6d62b635c7a8504120
Djovani Cotaj <justyehova@gmail.com>
2023-03-09 12:31:12 +0100

    We changed the code so that the timer is added and the reset button also works.

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -15,50 +15,113 @@ using System.Windows.Shapes;
 
 namespace GameWpf
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthOfSecondsElapsed;
+        int matchesFound;
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
         public MainWindow()
         {
             InitializeComponent();
-
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + "- Play again?";
+            }
         }
 
         private void SetUpGame()
         {
             List<string> animalList = new List<string>()
-            {
-                "🐵",
-                "🐵",
-                "👽",
-                "👽",
-                "🐶",
-                "🐶",
-                "🐰",
-                "🐰",
-                "🐨",
-                "🐨",
-                "🐔",
-                "🐔",
-                "🐴",
-                "🐴",
-                "🐘",
-                "🐘"
-            };
+    {
+        "🐵",
+        "🐵",
+        "👽",
+        "👽",
+        "🐶",
+        "🐶",
+        "🐰",
+        "🐰",
+        "🐨",
+        "🐨",
+        "🐔",
+        "🐔",
+        "🐴",
+        "🐴",
+        "🐘",
+        "🐘"
+    };
 
             Random random = new Random();
 
-            foreach (TextBlock textBlock in grid.Children.OfType<TextBlock>()) 
+            foreach (TextBlock textBlock in grid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalList.Count);
-                string nextEmoji = animalList[index];
-                textBlock.Text = nextEmoji;
-                animalList.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible; // reset visibility
+                    int index = random.Next(animalList.Count);
+                    Console.WriteLine(index);
+                    string nextEmoji = animalList[index];
+                    textBlock.Text = nextEmoji;
+                    animalList.RemoveAt(index);
+                }
             }
 
+            timer.Start();
+            tenthOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
         }
     }
 }
